855e728f4c1fe0f21bf9aa59ed8270bc36733d8b
ksmles36 <ksmles36@gmail.com>
2022-11-07 20:38:06 +0900

    Initial commit

---
+++ src/main/java/com/springboot/api/controller/GetController.java
@@ -0,0 +1,12 @@
+package com.springboot.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/get-api")
+public class GetController {
+
+    
+
+}
