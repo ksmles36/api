80016c7891160719c2ffb0b94727575b1cec34e1
ksmles36 <ksmles36@gmail.com>
2022-11-07 20:42:50 +0900

    a

--- src/main/java/com/springboot/api/controller/GetController.java
+++ src/main/java/com/springboot/api/controller/GetController.java
@@ -1,5 +1,6 @@
 package com.springboot.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/get-api")
 public class GetController {
 
-    
+    @GetMapping(value = "/name")
+    public String getName() {
+        return "Flature";
+    }
 
 }
